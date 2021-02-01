eea4b9030316b982682936c8235d0eea9dd53deb
Cole Enocks <enockscoleman@gmail.com>
2021-02-01 00:41:12 +0000

    implement modal display function

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
   const boardList = $("#board li");
+  const modalText = $(".modal-text");
+  const modalButton = $(".modal button");
+  const modalContainer = $(".modal-container");
   let turns = 0;
 
   let spot1 = $("#spot1");
@@ -50,16 +53,27 @@ $(document).ready(function () {
     }
   };
 
+  const displayModal = (msg) => {
+    modalContainer.show();
+    modalText.text(msg);
+  };
+
+  const closeModal = () => {
+    modalContainer.hide();
+  };
+
+  modalButton.on("click", closeModal);
+
   boardList.on("click", function () {
     if ($(this).hasClass("filled")) {
-      alert("this spot is filled!");
+      displayModal("This spot is filled!");
     } else if (turns % 2 === 0) {
       //even turns
       turns++;
       $(this).text("o");
       $(this).addClass("o filled");
       if (checkWinFor("o")) {
-        alert("O Won!");
+        displayModal("Player O Won!");
         refreshBoard();
         turns = 0;
       }
@@ -69,14 +83,14 @@ $(document).ready(function () {
       $(this).text("x");
       $(this).addClass("x filled");
       if (checkWinFor("x")) {
-        alert("X Won!");
+        displayModal("Player X Won!");
         refreshBoard();
         turns = 0;
       }
     }
     //No more turns
     if (turns === 9) {
-      alert("its a Tie!");
+      displayModal("Its a Tie!");
       refreshBoard();
       turns = 0;
     }
